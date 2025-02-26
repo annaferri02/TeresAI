dfd289583d8ebabc324859b4ea035c4b28cced59
ninafang123 <n.fang1@students.uu.nl>
2025-02-26 14:32:13 +0100

    change view report to transcription on transcription page

--- TeresAI/app/transcriptions/page.tsx
+++ TeresAI/app/transcriptions/page.tsx
@@ -64,7 +64,7 @@ export default function TranscriptionsPage() {
                       className="bg-custom-lilac text-white border-white hover:bg-white hover:text-custom-lilac"
                     >
                       <ExternalLink className="mr-2 h-4 w-4" />
-                      View Report
+                      View Transcription
                     </Button>
                   </Link>
                 )}
