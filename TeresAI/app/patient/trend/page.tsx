245de849f7b21105f1c606e2a0ec66f560d2ed7a
RobVer <58036019+RobVer@users.noreply.github.com>
2025-04-02 15:34:16 +0200

    updated trendpage

--- TeresAI/app/patient/trend/page.tsx
+++ TeresAI/app/patient/trend/page.tsx
@@ -1,7 +1,11 @@
 "use client";
-import { TrendingUp, TrendingDown } from "lucide-react"
+import { TrendingUp, TrendingDown, TrendingUpDown } from "lucide-react"
 import Link from "next/link";
-import {CustomButton} from "@/components/ui/custom-styles";
+import {CustomButton, CustomCard} from "@/components/ui/custom-styles";
+import TrendBox from "@/components/trendbox";
+import { Noto_Emoji } from "next/font/google";
+import { CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
 
 export default function TrendPage() {
   const trends = [
@@ -28,24 +32,21 @@ export default function TrendPage() {
         <h1 className="text-2xl font-bold">TeresAI</h1>
       </header>*/}
       <main className="flex min-h-screen flex-col items-center justify-start p-24 bg-white text-custom-lilac ">
-        <h2 className="text-3xl font-bold text-custom-dark-purple mb-6">Trends</h2>
-          <div className="space-y-4">
-            {trends.map((trend, index) => (
-                <div key={index} className="bg-light-purple-lilac p-4 bg-white rounded-lg shadow-lg border-custom-lilac flex items-start">
-                  <div className="mr-4 mt-1">
-                    {trend.direction === "increase" ? (
-                        <TrendingUp className="w-6 h-6 text-red-500"/>
-                    ) : (
-                        <TrendingDown className="w-6 h-6 text-green-500"/>
-                    )}
-                  </div>
-                  <div>
-                    <p className="text-dark-lilac mb-2">{trend.description}</p>
-                    <span className="text-2xl">{trend.emoji}</span>
-                  </div>
-                </div>
-            ))}
-          </div>
+        
+          <CustomCard>
+            <CardHeader>
+              <CardTitle className="flex items-center text-white text-custom-dark-purple gap-1"><TrendingUpDown />Trends</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <ul>
+                {trends.map((trend, index) => (
+                  <li className="my-4 text-custom-dark-purple">
+                    <TrendBox direction={trend.direction} description={trend.description} emoji={trend.emoji}></TrendBox>
+                  </li>
+                ))}
+              </ul>
+            </CardContent>
+          </CustomCard>
         <div className="mt-12 space-x-4">
           <Link href="/patient">
             <CustomButton variant="outline">Back to Patient's Dashboard</CustomButton>
